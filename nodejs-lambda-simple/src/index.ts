8ceaf69ed1787c61eb3b08e718a7cd45a4d83552
Luke Zhang <lukezha@amazon.com>
2025-02-02 08:11:13 -0800

    proj: copy node.js lambda sample app from aws-otel-js-instrumentation.

---
+++ nodejs-lambda-simple/src/index.ts
@@ -0,0 +1,26 @@
+console.log("start running sample code");
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from 'aws-lambda';
+
+import { S3 } from '@aws-sdk/client-s3';
+
+const s3 = new S3();
+
+exports.handler = async (_event: APIGatewayProxyEvent, _context: Context) => {
+  console.info('Serving lambda request.');
+
+  const result = await s3.listBuckets();
+
+  console.log('Fetched OTel Resource Attrs:' + process.env.OTEL_RESOURCE_ATTRIBUTES);
+  console.log('Fetched X-Ray Trace Header:' + process.env['_X_AMZN_TRACE_ID']);
+
+  const response: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: `Hello lambda - found ${result.Buckets?.length || 0} buckets. X-Ray Trace ID: ${process.env['_X_AMZN_TRACE_ID'] || 'Not available'}`,
+  };
+  return response;
+};
