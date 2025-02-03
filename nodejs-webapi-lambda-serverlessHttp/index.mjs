bfb98755aef1f7d6075660558ba5dd6eb5c0949c
Luke Zhang <lukezha@amazon.com>
2025-02-03 15:09:59 -0800

    proj: enable node.js lambda function with serverless-http.

--- nodejs-webapi-lambda-serverlessHttp/index.mjs
+++ nodejs-webapi-lambda-serverlessHttp/index.mjs
@@ -1,12 +1,11 @@
-import awsServerlessExpress from "aws-serverless-express";
+import serverless from "serverless-http"; 
 import webApp from "./src/webapi.mjs"
+
+
 
 // const port = 3000;
 // webApp.listen(port, () => {
 //     console.log(`Server is running at http://localhost:${port}`);
 // });
 
-const server = awsServerlessExpress.createServer(webApp);
-export const handler = (event, context) => {
-  awsServerlessExpress.proxy(server, event, context);
-};
+export const handler = serverless(webApp);
