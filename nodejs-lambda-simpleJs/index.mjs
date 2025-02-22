20d47866eb135d2febf773c330a1ec8ca1315827
Luke Zhang <lukezha@amazon.com>
2025-02-21 22:21:24 -0800

    b.

--- nodejs-lambda-simpleJs/index.mjs
+++ nodejs-lambda-simpleJs/index.mjs
@@ -1,17 +1,19 @@
-import webApp from "./src/webapi.mjs"
+import { callS3, pingWebSite } from "./src/webapi.mjs"
 
 export const handler = async (_event, _context) => {
     console.info('Serving lambda request.');
 
     console.info('Calling S3.');
-
+    callS3();
 
     console.info("Ping web site.")
-
+    pingWebSite();
 
     const response = {
         statusCode: 200,
-        body: bodyMsg
+        body: "Great Feature!"
     };
     return response;
 };
+
+handler();
--- nodejs-lambda-simpleJs/src/webapi.mjs
+++ nodejs-lambda-simpleJs/src/webapi.mjs
@@ -1,39 +1,29 @@
 import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
-import express from "express";
 import http from "http";
 
-const webApp = express();
-
-webApp.get('/', (req, res) => {
-    console.log(`sdkv3: handling call at root /.`);
-    res.send(`Welcome! Two web methods are provided:  /aws-sdk-call    and    /outgoing-http-call`);
-});
-
-webApp.get('/aws-sdk-call', (req, res) => {
+export function callS3() {
     console.log(`sdkv3: handling /aws-sdk-call with S3 call.`);
-    try
-    {
+    try {
         const s3Client = new S3Client();
         const command = new ListBucketsCommand({});
         s3Client.send(command)
             .then(function (data) {
-                res.json({
+                const result = JSON.stringify({
                     XRayTraceID: `${process.env["_X_AMZN_TRACE_ID"] || "Trace Id not available"}`,
                     S3Buckets: data.Buckets
                 });
+                console.log(result);
             })
             .catch(function (err) {
                 console.error(`Error in aws-sdk-call:  ${err.message}`);
-                res.status(500).send('Error listing S3 buckets: ' + err.message);
             });
     }
-    catch(err)
-    {
+    catch (err) {
         console.error(`error found in S3 call: ${err?.message || err}`);
     }
-});
+};
 
-webApp.get('/outgoing-http-call', (req, res) => {
+export function pingWebSite() {
     console.log(`sdkv3: handling /outgoing-http-call.`);
     const httpReq = http.get('http://aws.amazon.com', (httpResponse) => {
         console.log('Response status code:', httpResponse.statusCode);
@@ -45,17 +35,11 @@ webApp.get('/outgoing-http-call', (req, res) => {
         httpResponse.on('end', function () {
             console.log('Response body:');
             console.log(data); // Complete response body after all chunks
-            res.json({
-                XRayTraceID: `${process.env["_X_AMZN_TRACE_ID"] || "Trace Id not available"}`,
-                Message: "Pinged aws.amazon.com",
-                ResponseBody: data
-            });
         });
     })
 
     httpReq.on('error', function (err) {
         console.error(`Error in outgoing-http-call:  ${err.message}`);
     });
-});
+};
 
-export default webApp;
