0d8ee3756f6da0d13559c76cd50a7bcd2c6578fd
Luke Zhang <lukezha@amazon.com>
2025-02-22 15:12:21 -0800

    f

--- nodejs-lambda-simpleJs/index.mjs
+++ nodejs-lambda-simpleJs/index.mjs
@@ -3,14 +3,18 @@ import { callS3, pingWebSite } from "./src/webapi.mjs"
 export const handler = async (_event, _context) => {
     console.info('Serving lambda request.');
 
+    const eventStr = JSON.stringify(_event, null, 2);
+    console.log('Received xxxevent:', eventStr);
+    let bodyMsg = "xxxEvent:\r\n\r\n" + eventStr + "\r\n\r\n";
+
     console.info('Calling S3.');
     const responseA = await callS3();
 
     console.info("Ping web site.")
     const responseB = await pingWebSite();
 
-    let bodyMsg = "S3 call:\r\n\r\n" + responseA + "\r\n\r\n";
-    bodyMsg = bodyMsg + "Ping web site:\r\n\r\n" + responseB;
+    bodyMsg = bodyMsg + "S3 call:\r\n\r\n" + responseA;
+    bodyMsg = bodyMsg + "\r\n\r\n" + "Call web site:\r\n\r\n" + responseB;
     const response = {
         statusCode: 200,
         body: bodyMsg
