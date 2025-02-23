60f920fde58d53db0c469ef06c9df3be5674c47f
Luke Zhang <lukezha@amazon.com>
2025-02-23 08:21:08 -0800

    add parameters info from event and context.

--- nodejs-lambda-simpleJs/index.mjs
+++ nodejs-lambda-simpleJs/index.mjs
@@ -6,11 +6,12 @@ export async function handler(event, context) {
 
     console.info('xxx Start serving lambda request.');
 
-    const eventStr = JSON.stringify(event, null, 2);
-    console.log('Received xxxevent:', eventStr);
-    let bodyMsg = "xxxEvent:\r\n\r\n" + eventStr + "\r\n\r\n";
+    const paramEvent = "param-event:\r\n\r\n" + JSON.stringify(event, null, 2) + "\r\n\r\n";
+    const paramContext = "param-context:\r\n\r\n" + JSON.stringify(context, null, 2) + "\r\n\r\n";
 
     let retObj = await myHandler(event, context);
+
+    retObj.body += ("\r\n\r\n" + paramEvent + paramContext);
     return retObj;
 }
 
