d4b4b48701cd6cc29ac646ce3694a367f26cc413
Luke Zhang <lukezha@amazon.com>
2025-02-24 15:42:44 -0800

    add more web methods to my spring boot project.

--- springBootWithGradle/src/main/java/com/example/demo/DemoApplication.java
+++ springBootWithGradle/src/main/java/com/example/demo/DemoApplication.java
@@ -11,8 +11,35 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
+
+    @GetMapping("/")
+    public String home(String name) {
+        return """
+                <html>
+                <body>
+                    <p>Two web methods are provided:</p>
+                    <ul>
+                        <li><a href="/hello" target="_blank">/hello</a></li>
+                        <li><a href="/aws-sdk-call" target="_blank">/aws-sdk-call</a></li>
+                        <li><a href="/outgoing-http-call" target="_blank">/outgoing-http-call</a></li>
+                    </ul>
+                </body>
+                </html>
+               """;
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/aws-sdk-call")
+    public String awsSdkCall(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("aws-sdk-call: %s!", name);
+    }
+
+    @GetMapping("/outgoing-http-call")
+    public String outgoingHttpCall(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("outgoing-http-call: %s!", name);
     }
 }
