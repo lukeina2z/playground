8c12b80ac923d95dadc778a453eb770da1623fb9
Luke Zhang <lukezha@amazon.com>
2025-02-25 09:38:23 -0800

    spring boot now works on lambda.

--- springBootWithGradle/src/main/java/com/example/demo/StreamLambdaHandler.java
+++ springBootWithGradle/src/main/java/com/example/demo/StreamLambdaHandler.java
@@ -1,41 +1,31 @@
 package com.example.demo;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.amazonaws.serverless.exceptions.ContainerInitializationException; 
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 public class StreamLambdaHandler implements RequestStreamHandler {
-    private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    
     static {
         try {
-            logger.info("Handler initializing...");
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DemoApplication.class);
-            // Optional: Configure the handler with custom settings
-            /// handler.stripBasePath("/api"); // If you have a base path in API Gateway
-            /// 
-            
-            logger.info("Handler initialized");
         } catch (ContainerInitializationException e) {
-            logger.error("Could not initialize Spring Boot application", e);
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) 
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
         handler.proxyStream(inputStream, outputStream, context);
     }
