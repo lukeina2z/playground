14891b936b483bf2150eaa9ed02c5544be89fac8
Luke Zhang <lukezha@amazon.com>
2025-02-25 12:37:35 -0800

    remove unnecessary code.

--- lambdaWithSpringBoot/src/main/java/com/example/demo/DemoApplication.java
+++ lambdaWithSpringBoot/src/main/java/com/example/demo/DemoApplication.java
@@ -10,28 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpMethod;
 
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
-
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-
-import java.util.HashMap;
-
-record Greeting(long id, String content) {
-}
 
 @SpringBootApplication
 @RestController
@@ -58,18 +44,9 @@ public class DemoApplication {
                 """;
     }
 
-    @GetMapping("/hello")
-    public Greeting hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        // return String.format("Hello %s!", name);
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
-
     @GetMapping("/aws-sdk-call")
     public String awsSdkCall(@RequestParam(value = "name", defaultValue = "World") String name) {
-        // return String.format("aws-sdk-call: %s!", name);
-
-        // Create S3 client
-        Region region = Region.US_EAST_1; // or your preferred region
+        Region region = Region.US_WEST_2;
         S3Client s3 = S3Client.builder()
                 .region(region)
                 .build();
@@ -96,35 +73,28 @@ public class DemoApplication {
     @GetMapping(value = "/outgoing-http-call", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiResponse> outgoingHttpCall(String name) {
         try {
-            // Create RestTemplate instance
             RestTemplate restTemplate = new RestTemplate();
-
-            // Make the HTTP GET request to Google
             ResponseEntity<String> response = restTemplate.exchange(
                     "http://aws.amazon.com",
                     HttpMethod.GET,
                     null,
                     String.class);
 
-            // Create API response with the content and status
             ApiResponse apiResponse = new ApiResponse(
                     response.getBody(),
                     response.getStatusCode().value(),
                     "Success");
 
-            // Return successful response with status code 200
             return ResponseEntity
                     .ok()
                     .body(apiResponse);
 
         } catch (Exception e) {
-            // Create error response
             ApiResponse errorResponse = new ApiResponse(
                     null,
                     HttpStatus.INTERNAL_SERVER_ERROR.value(),
                     "Error: " + e.getMessage());
 
-            // Return error response with status code 500
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(errorResponse);
--- lambdaWithSpringBoot/src/main/java/com/example/demo/StreamLambdaHandler.java
+++ lambdaWithSpringBoot/src/main/java/com/example/demo/StreamLambdaHandler.java
@@ -11,14 +11,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DemoApplication.class);
         } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
