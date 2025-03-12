cb388a7b028d65bae05eb700ce1000b434d9184e
Luke Zhang <lukezha@amazon.com>
2025-03-12 08:24:26 -0700

    create a standalone spring boot web app.

---
+++ springBootWithGradle-NoLambda/src/main/java/com/example/demo/StreamLambdaHandler.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.internal.servlet.AwsProxyHttpServletRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
+
+    // LambdaLogger logger = null;
+    static {
+        try {
+            logger.info("xxxlogger: before initializing handler in the static world. ");
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DemoApplication.class);
+            logger.info("xxxlogger: after initializing handler in the static world. ");
+        } catch (ContainerInitializationException e) {
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        logger.info("xxxlogger: before proxyStream call. " + inputStream.toString());
+        handler.proxyStream(inputStream, outputStream, context);
+        logger.info("xxxlogger: after proxyStream call. " + outputStream.toString());;
+    }
+}
