39ba19cdcfb19542f772404f90ab28d2dd0c0fbd
Luke Zhang <lukezha@amazon.com>
2025-08-02 19:30:15 -0700

    flush logger

--- nodejs-lambda-simpleJs/index.js
+++ nodejs-lambda-simpleJs/index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+// flush with a bounded timeout so your Lambda doesn't hang forever
+async function flushLogs(provider, timeoutMs) {
+    return Promise.race([
+        provider.shutdown(), // triggers immediate export of pending logs
+        new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('forceFlush timed out')), timeoutMs)
+        )
+    ]);
+}
+
+async function flushLogger() {
+    try {
+        const { logs } = require('@opentelemetry/api-logs');
+        const globalLoggerProvider = logs.getLoggerProvider();
+        await flushLogs(globalLoggerProvider, 3000); // e.g., 3 seconds
+    } catch (err) {
+        console.warn('forceFlush failed or timed out:', err);
+    }
+}
+
 const autoInstrumentationTest = async () => {
     const autoMain = require('./initOTelAuto');
 
@@ -11,6 +31,8 @@ const autoInstrumentationTest = async () => {
 
     const traceTestFn = require('./tests/traceTest');
     await traceTestFn();
+
+    await flushLogger();
 
     const contextTest = require("./context-test");
     console.log(`The End`);
@@ -35,3 +57,6 @@ async function handler(event, context) {
 module.exports = { handler };
 
 // handler({ key1: 'value1', key2: 'value2' }, { /* context object */ });
+
+
+
