38e15ce9a799bd45cf2ccfb2eadb5b507c831e12
Luke Zhang <lukezha@amazon.com>
2025-08-30 12:17:14 -0700

    net app with otel nuget.

---
+++ .net/net-with-otel-nuget/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp;
+
+OTelTraceTest traceTest = new OTelTraceTest();
+
+var traceId = traceTest.AWSSDKCall();
+System.Console.WriteLine($"AWS Call tracke Id: {traceId}");
+
+traceId = traceTest.OutgoingHttp();
+System.Console.WriteLine($"Outgoing Http Call tracke Id: {traceId}");
+
+Console.WriteLine("Hello, World!");
---
+++ .net/net-with-otel-nuget/ConsoleAppWithNetFx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithNetFx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdotTestCalls tests = new AdotTestCalls();
+            var traceId = tests.AWSSDKCall();
+            System.Console.WriteLine($"AWS Call tracke Id: {traceId}");
+
+            traceId = tests.OutgoingHttp();
+            System.Console.WriteLine($"Outgoing Http Call tracke Id: {traceId}");
+
+            Console.WriteLine("The End!");
+        }
+    }
+}
