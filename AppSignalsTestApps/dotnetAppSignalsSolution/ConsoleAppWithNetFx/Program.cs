1b3ede1faf6cdd57c91baff64018a2d633d2e52c
Luke Zhang <lukezha@amazon.com>
2025-08-30 23:58:24 -0700

    update adot net testing app.

--- AppSignalsTestApps/dotnetAppSignalsSolution/ConsoleAppWithNetFx/Program.cs
+++ AppSignalsTestApps/dotnetAppSignalsSolution/ConsoleAppWithNetFx/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleAppWithNetFx
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static Timer _timer;
+
+        static void Foo()
+        {
+            // Create a timer that calls Bar() every 3 seconds (3000 ms)
+            _timer = new Timer(_ => Bar(), null, 0, 3000);
+        }
+
+        static void Bar()
         {
             AdotTestCalls tests = new AdotTestCalls();
             var traceId = tests.AWSSDKCall();
@@ -19,5 +24,14 @@ namespace ConsoleAppWithNetFx
 
             Console.WriteLine("The End!");
         }
+
+        static void Main(string[] args)
+        {
+            Foo();
+
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadLine();
+        }
     }
 }
+
