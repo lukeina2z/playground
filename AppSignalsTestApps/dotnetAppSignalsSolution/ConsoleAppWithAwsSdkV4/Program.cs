55ca4346acbfafa9982ff72f3324cb19bb23edbf
Luke Zhang <lukezha@amazon.com>
2025-08-31 21:53:27 -0700

    add awssdk v4 test.

---
+++ AppSignalsTestApps/dotnetAppSignalsSolution/ConsoleAppWithAwsSdkV4/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Tasks;
+using ConsoleApp;
+
+await MyProgram.Main(args);
+
+class MyProgram
+{
+    public static async Task Foo()
+    {
+        while (true)
+        {
+            Bar();
+            await Task.Delay(3000); // wait 3 seconds
+        }
+    }
+
+    static void Bar()
+    {
+        OTelTraceTest traceTest = new OTelTraceTest();
+
+        var traceId = traceTest.AWSSDKCall();
+        System.Console.WriteLine($"AWS Call tracke Id: {traceId}");
+
+        traceId = traceTest.OutgoingHttp();
+        System.Console.WriteLine($"Outgoing Http Call tracke Id: {traceId}");
+
+        Console.WriteLine("\n");
+    }
+
+    internal static async Task Main(string[] args)
+    {
+        await Foo();
+    }
+}
+
+
