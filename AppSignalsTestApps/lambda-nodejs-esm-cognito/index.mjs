870e5c3ee8b1ebd1cf75b51bfac77b7fb23db04d
Luke Zhang <lukezha@amazon.com>
2025-09-11 15:29:19 -0700

    a

--- AppSignalsTestApps/lambda-nodejs-esm-cognito/index.mjs
+++ AppSignalsTestApps/lambda-nodejs-esm-cognito/index.mjs
@@ -1,12 +1,12 @@
-import { myHandler } from "./autoInstrumentOnly.mjs";
+import { myHandler  } from "./myHandler.mjs";
 
 // Remove testing call in Lambda
 const test = async () => {
     const rsp = await myHandler({}, {});
     console.log("Done");
 }
-
 test();
+// Remove testing call in Lambda
 
 export { myHandler };
 
--- AppSignalsTestApps/lambda-nodejs-esm-cognito/myHandler.mjs
+++ AppSignalsTestApps/lambda-nodejs-esm-cognito/myHandler.mjs
@@ -43,3 +43,17 @@ export function pingWebSite() {
         });
     });
 };
+
+export const myHandler = async (_event, _context) => {
+    const responseA = await callS3();
+    const responseB = await pingWebSite();
+
+    let bodyMsg = "S3 call:\r\n\r\n" + responseA + "\r\n\r\n";
+    bodyMsg = bodyMsg + "Ping web site:\r\n\r\n" + responseB;
+    const response = {
+        statusCode: 200,
+        body: bodyMsg
+    };
+    return response;
+};
+
