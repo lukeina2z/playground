81adc3b58e1b1080c744d22d65d987115467d833
Luke Zhang <lukezha@amazon.com>
2025-10-02 18:05:53 -0700

    mcp test

--- .net/mcp/McpCalculatorServer/Program.cs
+++ .net/mcp/McpCalculatorServer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+[McpServerToolType]
+public static class CalculatorTool
+{
+    [McpServerTool, Description("Adds two numbers")]
+    public static string Add(int a, int b) => $"Sum {a + b}";
+}
+
+
