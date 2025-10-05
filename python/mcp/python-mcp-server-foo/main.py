e96cda636f9586d002ef6de4060f32f1b275c16e
Luke Zhang <lukezha@amazon.com>
2025-10-05 11:53:04 -0700

    add mcp testing code.

--- python/mcp/python-mcp-server-foo/main.py
+++ python/mcp/python-mcp-server-foo/main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from python-mcp-server-foo!")
+# def main():
+#     print("Hello from python-mcp-server-foo!")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+
+# server.py
+
+
+import debugpy
+debugpy.listen(5678)
+print("Waiting for debugger attach on port 5678...")
+debugpy.wait_for_client()
+print("Debugger attached!")
+
+
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("PythonMcpDemoFoo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+
+
