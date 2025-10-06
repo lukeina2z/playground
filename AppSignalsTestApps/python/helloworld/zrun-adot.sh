175ad45c4a54c8d4665b159cebe6ebc56e77fc8f
Luke Zhang <lukezha@amazon.com>
2025-10-06 10:56:08 -0700

    enable python adot agent.

--- AppSignalsTestApps/python/helloworld/zrun-adot.sh
+++ AppSignalsTestApps/python/helloworld/zrun-adot.sh
@@ -1,8 +1,12 @@
-#!bash
+#!/bin/bash
 
+### ADOT
+
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Application-Signals-Enable-EC2Main.html#CloudWatch-Application-Signals-Enable-Other-instrument
+# python -m venv v-adot
+# source ./v-adot/bin/activate
 # pip install boto3
 # pip install aws-opentelemetry-distro
-
 
 
 OTEL_METRICS_EXPORTER=none \
@@ -10,26 +14,13 @@ OTEL_LOGS_EXPORTER=none \
 OTEL_AWS_APPLICATION_SIGNALS_ENABLED=true \
 OTEL_PYTHON_DISTRO=aws_distro \
 OTEL_PYTHON_CONFIGURATOR=aws_configurator \
-OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
+OTEL_EXPORTER_OTLP_PROTOCOL=grpc \
+# OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
 OTEL_TRACES_SAMPLER=xray \
 OTEL_TRACES_SAMPLER_ARG="endpoint=http://localhost:2000" \
 OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT=http://localhost:4316/v1/metrics \
-OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
-OTEL_RESOURCE_ATTRIBUTES="service.name=python-temp2" \
+# OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
+OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://xyz-jaeger-100:4317/v1/traces \
+OTEL_RESOURCE_ATTRIBUTES="service.name=python-ADOT-EnvVar" \
 opentelemetry-instrument python ./adot-test.py
 
-# OTEL_METRICS_EXPORTER=none \
-# OTEL_LOGS_EXPORTER=none \
-# OTEL_AWS_APPLICATION_SIGNALS_ENABLED=true \
-# OTEL_PYTHON_DISTRO=aws_distro \
-# OTEL_PYTHON_CONFIGURATOR=aws_configurator \
-# OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
-# OTEL_TRACES_SAMPLER=always_on \
-# OTEL_LOG_LEVEL=info \
-# # OTEL_TRACES_SAMPLER_ARG="endpoint=http://localhost:2000" \
-# # OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT=http://localhost:4316/v1/metrics \
-# OTEL_TRACES_EXPORTER=otlp \
-# # OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://100.21.71.227:4318/v1/traces \
-# OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
-# OTEL_RESOURCE_ATTRIBUTES="service.name=python-adot-demo" \
-# opentelemetry-instrument python ./adot-test.py
