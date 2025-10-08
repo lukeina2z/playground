5d5e8e97dfb78d13ce0dfc1d95fe2fb1d56998a3
Luke Zhang <lukezha@amazon.com>
2025-10-07 21:28:00 -0700

    add root span in mcp client.

--- .net/mcp-adot/mcp-adot-client/Program.cs
+++ .net/mcp-adot/mcp-adot-client/Program.cs
@@ -7,62 +7,70 @@ using System.Diagnostics;
 using System.Net;
 using System.Text;
 
-var builder = Host.CreateApplicationBuilder(args);
+// Add OpenTelemetry using
+using OpenTelemetry;
+using OpenTelemetry.Trace;
 
-builder.Configuration 
-    .AddEnvironmentVariables()
-    .AddUserSecrets<Program>();
+var activitySource = new ActivitySource("mcp-adot-client");
 
-var clientTransport = new StdioClientTransport(new()
+using (var activity = activitySource.StartActivity("MainProgram"))
 {
-    Name = "Demo Server",
-    Command = $"{Path.Combine(AppContext.BaseDirectory, "../../../../", "mcp-adot-server/bin/Debug/net9.0/mcp-adot-server.exe")}",
-    Arguments = [],
-    EnvironmentVariables = new Dictionary<string, string?>
+    // All your code goes here
+    var builder = Host.CreateApplicationBuilder(args);
+
+    builder.Configuration
+        .AddEnvironmentVariables()
+        .AddUserSecrets<Program>();
+
+    var clientTransport = new StdioClientTransport(new()
     {
-        { "OTEL_DOTNET_AUTO_TRACES_ADDITIONAL_SOURCES", "Experimental.ModelContextProtocol" },
-        {"OTEL_RESOURCE_ATTRIBUTES", "service.name=MCP-Srv-DotNET"},
-        { "OTEL_METRICS_EXPORTER", "none"},
-        { "OTEL_LOGS_EXPORTER", "otlp"},
-        { "OTEL_EXPORTER_OTLP_TRACES_ENDPOINT", "http://localhost:4318/v1/traces"},
-        { "OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf"},
-        { "OTEL_TRACES_SAMPLER", "always_on"},
-        {"CORECLR_ENABLE_PROFILING", "1" },
-        { "CORECLR_PROFILER", "{918728DD-259F-4A6A-AC2B-B85E1B658318}"},
-        { "CORECLR_PROFILER_PATH_64", "c:/ztest/OpenTelemetryDistribution/win-x64/OpenTelemetry.AutoInstrumentation.Native.dll"},
-        { "DOTNET_ADDITIONAL_DEPS", "c:/ztest/OpenTelemetryDistribution/AdditionalDeps"},
-        { "DOTNET_SHARED_STORE", "c:/ztest/OpenTelemetryDistribution/store"},
-        { "DOTNET_STARTUP_HOOKS", "c:/ztest/OpenTelemetryDistribution/net/OpenTelemetry.AutoInstrumentation.StartupHook.dll"},
-        { "OTEL_DOTNET_AUTO_HOME", "c:/ztest/OpenTelemetryDistribution"}
+        Name = "Demo Server",
+        Command = $"{Path.Combine(AppContext.BaseDirectory, "../../../../", "mcp-adot-server/bin/Debug/net9.0/mcp-adot-server.exe")}",
+        Arguments = [],
+        EnvironmentVariables = new Dictionary<string, string?>
+        {
+            { "OTEL_DOTNET_AUTO_TRACES_ADDITIONAL_SOURCES", "Experimental.ModelContextProtocol" },
+            {"OTEL_RESOURCE_ATTRIBUTES", "service.name=MCP-Srv-DotNET"},
+            { "OTEL_METRICS_EXPORTER", "none"},
+            { "OTEL_LOGS_EXPORTER", "otlp"},
+            { "OTEL_EXPORTER_OTLP_TRACES_ENDPOINT", "http://localhost:4318/v1/traces"},
+            { "OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf"},
+            { "OTEL_TRACES_SAMPLER", "always_on"},
+            {"CORECLR_ENABLE_PROFILING", "1" },
+            { "CORECLR_PROFILER", "{918728DD-259F-4A6A-AC2B-B85E1B658318}"},
+            { "CORECLR_PROFILER_PATH_64", "c:/ztest/OpenTelemetryDistribution/win-x64/OpenTelemetry.AutoInstrumentation.Native.dll"},
+            { "DOTNET_ADDITIONAL_DEPS", "c:/ztest/OpenTelemetryDistribution/AdditionalDeps"},
+            { "DOTNET_SHARED_STORE", "c:/ztest/OpenTelemetryDistribution/store"},
+            { "DOTNET_STARTUP_HOOKS", "c:/ztest/OpenTelemetryDistribution/net/OpenTelemetry.AutoInstrumentation.StartupHook.dll"},
+            { "OTEL_DOTNET_AUTO_HOME", "c:/ztest/OpenTelemetryDistribution"}
+        }
+    });
+
+    Console.WriteLine("Setting up stdio transport");
+
+    await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+
+    Console.WriteLine("Listing tools");
+
+    var tools = await mcpClient.ListToolsAsync();
+
+    foreach (var tool in tools)
+    {
+        Console.WriteLine($"Connected to server with tools: {tool.Name}");
     }
-});
 
-Console.WriteLine("Setting up stdio transport");
+    _ = await mcpClient.CallToolAsync(
+        "ping_web",
+        cancellationToken: CancellationToken.None);
 
-await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+    var result = await mcpClient.CallToolAsync(
+        "add",
+        new Dictionary<string, object?>() { ["a"] = 1, ["b"] = 3 },
+        cancellationToken: CancellationToken.None);
 
-Console.WriteLine("Listing tools");
+    Console.WriteLine("Result: " + ((TextContentBlock)result.Content[0]).Text);
 
-var tools = await mcpClient.ListToolsAsync();
+    System.Threading.Thread.Sleep(5000);
 
-foreach (var tool in tools)
-{
-    Console.WriteLine($"Connected to server with tools: {tool.Name}");
+    Console.WriteLine("Bye!");
 }
-
-_ = await mcpClient.CallToolAsync(
-    "ping_web",
-    cancellationToken: CancellationToken.None);
-
-
-var result = await mcpClient.CallToolAsync(
-    "add",
-    new Dictionary<string, object?>() { ["a"] = 1, ["b"] = 3  },
-    cancellationToken:CancellationToken.None);
-
-Console.WriteLine("Result: " + ((TextContentBlock)result.Content[0]).Text);
-
-System.Threading.Thread.Sleep(5000);
-
-Console.WriteLine("Bye!");
-
