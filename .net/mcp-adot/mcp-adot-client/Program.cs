9961ac0fcb6987153b3b2ef214ee6e8c7bbc77a6
Luke Zhang <lukezha@amazon.com>
2025-10-07 21:16:42 -0700

    dotnet mcp

---
+++ .net/mcp-adot/mcp-adot-client/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Client;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Protocol;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration 
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>();
+
+var clientTransport = new StdioClientTransport(new()
+{
+    Name = "Demo Server",
+    Command = $"{Path.Combine(AppContext.BaseDirectory, "../../../../", "mcp-adot-server/bin/Debug/net9.0/mcp-adot-server.exe")}",
+    Arguments = [],
+    EnvironmentVariables = new Dictionary<string, string?>
+    {
+        { "OTEL_DOTNET_AUTO_TRACES_ADDITIONAL_SOURCES", "Experimental.ModelContextProtocol" },
+        {"OTEL_RESOURCE_ATTRIBUTES", "service.name=MCP-Srv-DotNET"},
+        { "OTEL_METRICS_EXPORTER", "none"},
+        { "OTEL_LOGS_EXPORTER", "otlp"},
+        { "OTEL_EXPORTER_OTLP_TRACES_ENDPOINT", "http://localhost:4318/v1/traces"},
+        { "OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf"},
+        { "OTEL_TRACES_SAMPLER", "always_on"},
+        {"CORECLR_ENABLE_PROFILING", "1" },
+        { "CORECLR_PROFILER", "{918728DD-259F-4A6A-AC2B-B85E1B658318}"},
+        { "CORECLR_PROFILER_PATH_64", "c:/ztest/OpenTelemetryDistribution/win-x64/OpenTelemetry.AutoInstrumentation.Native.dll"},
+        { "DOTNET_ADDITIONAL_DEPS", "c:/ztest/OpenTelemetryDistribution/AdditionalDeps"},
+        { "DOTNET_SHARED_STORE", "c:/ztest/OpenTelemetryDistribution/store"},
+        { "DOTNET_STARTUP_HOOKS", "c:/ztest/OpenTelemetryDistribution/net/OpenTelemetry.AutoInstrumentation.StartupHook.dll"},
+        { "OTEL_DOTNET_AUTO_HOME", "c:/ztest/OpenTelemetryDistribution"}
+    }
+});
+
+Console.WriteLine("Setting up stdio transport");
+
+await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+
+Console.WriteLine("Listing tools");
+
+var tools = await mcpClient.ListToolsAsync();
+
+foreach (var tool in tools)
+{
+    Console.WriteLine($"Connected to server with tools: {tool.Name}");
+}
+
+_ = await mcpClient.CallToolAsync(
+    "ping_web",
+    cancellationToken: CancellationToken.None);
+
+
+var result = await mcpClient.CallToolAsync(
+    "add",
+    new Dictionary<string, object?>() { ["a"] = 1, ["b"] = 3  },
+    cancellationToken:CancellationToken.None);
+
+Console.WriteLine("Result: " + ((TextContentBlock)result.Content[0]).Text);
+
+System.Threading.Thread.Sleep(5000);
+
+Console.WriteLine("Bye!");
+
---
+++ .net/mcp-adot/mcp-adot-server/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+using System.Diagnostics;
+
+//if (!Debugger.IsAttached)
+//{
+//    Debugger.Launch(); // prompts to attach a debugger
+//}
+
+//// OR force a break if already attached
+//Debugger.Break();
+
+//HttpClient httpClient = new HttpClient();
+//_ = httpClient.GetAsync("http://www.aws.com").Result;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class CalculatorTool
+{
+    [McpServerTool, Description("Adds two numbers")]
+    public static string Add(int a, int b)
+    {
+
+        //HttpClient httpClient = new HttpClient();
+        //var ret = httpClient.GetAsync("http://www.amazon.com").Result;
+        return $"Sum {a + b}.";
+    }
+
+    [McpServerTool, Description("Ping web site with HTTP GET")]
+    public static string PingWeb()
+    {
+        return MakeHtttpCall();
+    }
+
+    static string MakeHtttpCall()
+    {
+        HttpClient httpClient = new HttpClient();
+        var ret =  httpClient.GetAsync("http://www.aws.com").Result;
+        return ret.ToString();
+    }
+}
