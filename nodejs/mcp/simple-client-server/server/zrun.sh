b3f545b2fceba93e61bfefd1324d30cab8727f4d
Luke Zhang <lukezha@amazon.com>
2025-10-09 12:22:54 -0700

    update server service name.

--- nodejs/mcp/simple-client-server/server/zrun.sh
+++ nodejs/mcp/simple-client-server/server/zrun.sh
@@ -9,7 +9,7 @@ npm install --save @opentelemetry/auto-instrumentations-node
 
 npm run build
 
-OTEL_SERVICE_NAME=Mcp-Client-zRun \
+OTEL_SERVICE_NAME=Mcp-SERVER-zRun \
   OTEL_LOG_LEVEL=all \
   OTEL_TRACES_EXPORTER=otlp \
   OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://xyz-jaeger-100:4318/v1/traces \
