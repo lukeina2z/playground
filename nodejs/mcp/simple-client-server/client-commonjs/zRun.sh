51993ae6dc77efa4cabd1039080a43bd73c59168
Luke Zhang <lukezha@amazon.com>
2025-10-10 12:00:22 -0700

    use local node agent.

--- nodejs/mcp/simple-client-server/client-commonjs/zRun.sh
+++ nodejs/mcp/simple-client-server/client-commonjs/zRun.sh
@@ -6,8 +6,13 @@
 
 # https://github.com/open-telemetry/opentelemetry-js/blob/main/doc/esm-support.md
 
-npm install --save @opentelemetry/api
-npm install --save @opentelemetry/auto-instrumentations-node
+# npm install --save @opentelemetry/api
+# npm install --save @opentelemetry/auto-instrumentations-node
+
+rm -fr ./build
+rm -fr ./node_modules
+
+npm install
 
 npm run build
 
@@ -15,8 +20,10 @@ npm run build
   #   --experimental-loader=@opentelemetry/instrumentation/hook.mjs   \
 
 OTEL_SERVICE_NAME=Mcp-Client-CJS-zRun \
+  OTEL_LOG_LEVEL=all \
+  OTEL_NODE_RESOURCE_DETECTORS="aws,env,host,os" \
   OTEL_TRACES_EXPORTER=otlp \
   OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://xyz-jaeger-100:4318/v1/traces \
-  node  --import @opentelemetry/auto-instrumentations-node/register  \
+  node  --require @opentelemetry/auto-instrumentations-node/register  \
   ./build/client.js
   
