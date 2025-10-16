e3e1608be8426372184e8105ed0970bf8858588c
Luke Zhang <lukezha@amazon.com>
2025-10-16 14:21:56 -0700

    nodejs otel project.

---
+++ AppSignalsTestApps/app-signals-nodejs-OTel/index.js
@@ -0,0 +1,22 @@
+const testingApp = require("./src/app");
+const {recordRequestMetric }= require("./src/metric");
+
+// const awsCalls = require('./src/awsS3Test');
+
+async function runForever() {
+  let keepRunning = true;
+  while (keepRunning) {
+    recordRequestMetric();
+    await testingApp();
+    // await awsCalls.s3Call();
+    // await sleep(10000);  // Wait for 1 second
+    // keepRunning = false;
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Start the loop
+runForever();
