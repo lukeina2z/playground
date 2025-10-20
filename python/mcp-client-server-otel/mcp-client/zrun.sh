d53fa44527937e8c02c1a82900f776568e20f609
Luke Zhang <lukezha@amazon.com>
2025-10-20 15:01:54 -0700

    delete python mcp projects.

--- python/mcp-client-server-otel/mcp-client/zrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### OTel
-
-    OTEL_SERVICE_NAME=mcp-client-PyShell \
-    OTEL_TRACES_EXPORTER=otlp \
-    OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://xyz-jaeger-100:4317/v1/traces \
-    uv run opentelemetry-instrument python ./main.py
