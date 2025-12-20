36701f10f34515ccfd201bf26864456d065c7d54
Luke Zhang <lukeina2z@outlook.com>
2025-12-19 22:21:00 -0800

    add com class which implements ICalculator.

--- atl/VisualStudioAtlSolution/ATLProjectDLLFoo/ATLProjectDLLFoo_i.h
+++ atl/VisualStudioAtlSolution/ATLProjectDLLFoo/ATLProjectDLLFoo_i.h
@@ -30,6 +30,10 @@
 #error this stub requires an updated version of <rpcndr.h>
 #endif /* __RPCNDR_H_VERSION__ */
 
+#ifndef COM_NO_WINDOWS_H
+#include "windows.h"
+#include "ole2.h"
+#endif /*COM_NO_WINDOWS_H*/
 
 #ifndef __ATLProjectDLLFoo_i_h__
 #define __ATLProjectDLLFoo_i_h__
@@ -48,13 +52,160 @@
 
 /* Forward Declarations */ 
 
+#ifndef __ICalculator_FWD_DEFINED__
+#define __ICalculator_FWD_DEFINED__
+typedef interface ICalculator ICalculator;
+
+#endif 	/* __ICalculator_FWD_DEFINED__ */
+
+
+#ifndef __Calculator_FWD_DEFINED__
+#define __Calculator_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class Calculator Calculator;
+#else
+typedef struct Calculator Calculator;
+#endif /* __cplusplus */
+
+#endif 	/* __Calculator_FWD_DEFINED__ */
+
+
 /* header files for imported files */
 #include "oaidl.h"
 #include "ocidl.h"
+#include "shobjidl.h"
 
 #ifdef __cplusplus
 extern "C"{
 #endif 
+
+
+#ifndef __ICalculator_INTERFACE_DEFINED__
+#define __ICalculator_INTERFACE_DEFINED__
+
+/* interface ICalculator */
+/* [unique][nonextensible][dual][uuid][object] */ 
+
+
+EXTERN_C const IID IID_ICalculator;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("0535e1b4-a566-403d-905a-dad5863cdafb")
+    ICalculator : public IDispatch
+    {
+    public:
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct ICalculatorVtbl
+    {
+        BEGIN_INTERFACE
+        
+        DECLSPEC_XFGVIRT(IUnknown, QueryInterface)
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            ICalculator * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        DECLSPEC_XFGVIRT(IUnknown, AddRef)
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            ICalculator * This);
+        
+        DECLSPEC_XFGVIRT(IUnknown, Release)
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            ICalculator * This);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetTypeInfoCount)
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
+            ICalculator * This,
+            /* [out] */ UINT *pctinfo);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetTypeInfo)
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
+            ICalculator * This,
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ ITypeInfo **ppTInfo);
+        
+        DECLSPEC_XFGVIRT(IDispatch, GetIDsOfNames)
+        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
+            ICalculator * This,
+            /* [in] */ REFIID riid,
+            /* [size_is][in] */ LPOLESTR *rgszNames,
+            /* [range][in] */ UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ DISPID *rgDispId);
+        
+        DECLSPEC_XFGVIRT(IDispatch, Invoke)
+        /* [local] */ HRESULT ( STDMETHODCALLTYPE *Invoke )( 
+            ICalculator * This,
+            /* [annotation][in] */ 
+            _In_  DISPID dispIdMember,
+            /* [annotation][in] */ 
+            _In_  REFIID riid,
+            /* [annotation][in] */ 
+            _In_  LCID lcid,
+            /* [annotation][in] */ 
+            _In_  WORD wFlags,
+            /* [annotation][out][in] */ 
+            _In_  DISPPARAMS *pDispParams,
+            /* [annotation][out] */ 
+            _Out_opt_  VARIANT *pVarResult,
+            /* [annotation][out] */ 
+            _Out_opt_  EXCEPINFO *pExcepInfo,
+            /* [annotation][out] */ 
+            _Out_opt_  UINT *puArgErr);
+        
+        END_INTERFACE
+    } ICalculatorVtbl;
+
+    interface ICalculator
+    {
+        CONST_VTBL struct ICalculatorVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define ICalculator_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define ICalculator_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define ICalculator_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#define ICalculator_GetTypeInfoCount(This,pctinfo)	\
+    ( (This)->lpVtbl -> GetTypeInfoCount(This,pctinfo) ) 
+
+#define ICalculator_GetTypeInfo(This,iTInfo,lcid,ppTInfo)	\
+    ( (This)->lpVtbl -> GetTypeInfo(This,iTInfo,lcid,ppTInfo) ) 
+
+#define ICalculator_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)	\
+    ( (This)->lpVtbl -> GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId) ) 
+
+#define ICalculator_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)	\
+    ( (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) ) 
+
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __ICalculator_INTERFACE_DEFINED__ */
 
 
 
@@ -66,6 +217,14 @@ extern "C"{
 
 
 EXTERN_C const IID LIBID_ATLProjectDLLFooLib;
+
+EXTERN_C const CLSID CLSID_Calculator;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("aba7fe24-4d6d-4290-b8e3-7449b120d560")
+Calculator;
+#endif
 #endif /* __ATLProjectDLLFooLib_LIBRARY_DEFINED__ */
 
 /* Additional Prototypes for ALL interfaces */
---
+++ atl/VisualStudioAtlSolution/ATLProjectDLLFoo/Calculator.cpp
@@ -0,0 +1,22 @@
+// Calculator.cpp : Implementation of CCalculator
+
+#include "pch.h"
+#include "Calculator.h"
+
+
+// CCalculator
+
+STDMETHODIMP CCalculator::InterfaceSupportsErrorInfo(REFIID riid)
+{
+	static const IID* const arr[] = 
+	{
+		&IID_ICalculator
+	};
+
+	for (int i=0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		if (InlineIsEqualGUID(*arr[i],riid))
+			return S_OK;
+	}
+	return S_FALSE;
+}
