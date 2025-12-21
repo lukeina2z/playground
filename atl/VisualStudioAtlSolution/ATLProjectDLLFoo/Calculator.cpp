59dd11ba21243f7e1f96654b37db20901fa07db3
Luke Zhang <lukeina2z@outlook.com>
2025-12-21 14:47:50 -0800

    add code to test string conversion functions.

--- atl/VisualStudioAtlSolution/ATLProjectDLLFoo/Calculator.cpp
+++ atl/VisualStudioAtlSolution/ATLProjectDLLFoo/Calculator.cpp
@@ -2,6 +2,7 @@
 
 #include "pch.h"
 #include "Calculator.h"
+#include <atlconv.h>
 
 
 // CCalculator
@@ -21,6 +22,37 @@ STDMETHODIMP CCalculator::InterfaceSupportsErrorInfo(REFIID riid)
 	return S_FALSE;
 }
 
+void Test(LPCSTR str)
+{
+	// Use ATL debug trace to dump the incoming parameter.
+	// Safe-print: handle null pointer.
+	if (str == nullptr)
+	{
+		ATLTRACE("Test: parameter is NULL\n");
+	}
+	else
+	{
+		ATLTRACE("Test: parameter = \"%s\"\n", str);    
+	}
+}
+
+void Test2(LPCSTR str)
+{
+    // Use ATL conversion helper to convert ANSI char string to OLE (wide) string
+    if (str == nullptr)
+    {
+        ATLTRACE("Test2: parameter is NULL\n");
+        return;
+    }
+
+    CA2CT tStr(str);
+
+    CT2COLE oleStr(tStr);
+    LPCOLESTR pOle = static_cast<LPCOLESTR>(oleStr);
+    // Print the converted OLE string (wide) to the debugger output
+    ATLTRACE("Test2: parameter (OLE) = \"%ls\"\n", pOle);
+}
+
 STDMETHODIMP CCalculator::Add(LONG a, LONG b, LONG* ret)
 {
     // TODO: Add your implementation code here
@@ -30,6 +62,9 @@ STDMETHODIMP CCalculator::Add(LONG a, LONG b, LONG* ret)
     {
         return E_FAIL;
     }
+
+    Test2("test2, Hello from Add method!");
+    Test("test, Hello from Add method!");
 
     *ret = result;
     return S_OK;
