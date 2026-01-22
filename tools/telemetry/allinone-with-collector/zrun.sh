9649387c1ba0de10186fc732cc0c35dfa597f920
Luke Zhang <lukeina2z@outlook.com>
2026-01-22 06:41:13 +0000

    sh file

--- tools/telemetry/allinone-with-collector/zrun.sh
+++ tools/telemetry/allinone-with-collector/zrun.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-docker compose -f ./docker-compose-jaeger.yml up --remove-orphans
 
-# --remove-orphans
+# docker ps -a
+
+# docker rm -f jaeger
+
+# docker start jaeger
+# docker stop jaeger
+# docker rm jaeger
+
+
+
+docker compose -f .\docker-compose.yml up --remove-orphans
+
+
