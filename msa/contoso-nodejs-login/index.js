03a443f0ac4c03490f94dc35ac48e1796a818ef0
Luke Zhang (GuangHui) <lukezhang@microsoft.com>
2026-02-19 11:56:50 -0800

    refactor traffic simulation logic and update runForever function

--- msa/contoso-nodejs-login/index.js
+++ msa/contoso-nodejs-login/index.js
@@ -1,12 +1,13 @@
-const testingApp = require("./src/app");
+const { main: testingApp, simulateTraffic } = require("./src/app");
 // const {recordRequestMetric }= require("./src/metric");
 
 async function runForever() {
   let keepRunning = true;
   while (keepRunning) {
     // recordRequestMetric();
-    await testingApp();
-    await sleep(60000);  // Wait for 1 second
+    await simulateTraffic();
+    const cooldown = Math.floor(Math.random() * 10000) + 5000; // 5-15 seconds
+    await sleep(cooldown);
     // keepRunning = false;
   }
 }
