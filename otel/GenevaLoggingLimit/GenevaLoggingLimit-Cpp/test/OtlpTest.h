66475a39bff74eee04ca41d33b6215cd06af761d
Luke Zhang <lukezhang@microsoft.com>
2026-03-16 22:25:07 -0700

    geneva test

---
+++ otel/GenevaLoggingLimit/GenevaLoggingLimit-Cpp/test/OtlpTest.h
@@ -0,0 +1,8 @@
+namespace MsaLab { namespace Details
+{
+  void TestLogWithOtlp();
+
+  void TestTraceWithOtlp();
+} // namespace Details
+} // namespace MsaLab
+
---
+++ otel/GenevaLoggingLimit/GenevaLoggingLimit-DotNet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenevaLoggingLimit_DotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
