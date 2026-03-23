eb1f7b0cda0b7cae372b512feab48a199bf2c78b
Luke Zhang <lukezhang@microsoft.com>
2026-03-22 22:17:30 -0700

    Add MSBuild project and source files for NoHibernation integration

---
+++ cpp/Hibernation/cl/cl.cpp
@@ -0,0 +1,12 @@
+// cl.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "../NoHibernation/NoHibernation.h"
+
+#pragma comment(lib, "NoHibernation.lib")
+
+int main()
+{
+    CNoHibernation noHibernation;
+    noHibernation.Run();
+}
