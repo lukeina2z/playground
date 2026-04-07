3c42ead357eefebc5b83d3bfc8a742bfdf27e46e
Luke Zhang <lukezhang@microsoft.com>
2026-04-07 08:11:15 -0700

    add kusto sample app.

---
+++ kusto/KustoAppFoo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KustoAppFoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
