7a2543a92b78f14c8b44db26dc0b446829e89f66
Luke Zhang <lukezhang@microsoft.com>
2026-06-10 20:25:13 -0700

    Remove unused OTelPipeline files and update main test to utilize Geneva ETW exporter for logging and tracing

--- otel/Geneva-ETW-Dll-Exe/GenevaEtwExe/test/OtlpTest.h
+++
@@ -1,8 +0,0 @@
-namespace MsaLab { namespace Details
-{
-  void TestLogWithOtlp();
-
-  void TestTraceWithOtlp();
-} // namespace Details
-} // namespace MsaLab
-
--- otel/Geneva-ETW-Dll-Exe/GenevaEtwExe/test/main.cpp
+++ otel/Geneva-ETW-Dll-Exe/GenevaEtwExe/test/main.cpp
@@ -5,24 +5,37 @@
 #include <iostream>
 #include <thread>
 
-#include "GenevaTest.h"
-#include "OtlpTest.h"
+#include "../../GenevaEtwDll/MsaTelemetry.h"
 
-void run_all_tests()
+void TestLogWithGenevaEtwExporter()
 {
-  // MsaLab::Details::TestLogWithGeneva();
- 
-  MsaLab::Details::TestTraceWithGeneva();
+    auto logger = MSA::Telemetry::GetLoggerForDataPoint();
+    logger->Fatal("Hello <Fatal> Otlp!");
+    logger->Error("Hello <Error> Otlp!");
+    logger->Info("Hello <Info> Otlp!");
+    logger->Debug("xyzxyz - The end. No trace context expected.");
+}
+
+void TestTraceWithGenevaEtwExporter()
+{
+    static auto tracer = MSA::Telemetry::GetTracer();
+    trace_api::StartSpanOptions optionsS2;
+    optionsS2.kind = trace_api::SpanKind::kClient;
+
+    // Create Span with 1 SpanLink
+    auto s2 = tracer->StartSpan("xyxyxy-Span", opentelemetry::common::MakeAttributes({ {"key1", "value 1"}, {"key2", 1} }), optionsS2);
+
+    s2->SetAttribute("attr_key1", 123);
+
+    s2->End();
 }
 
 int main()
 {
+    TestLogWithGenevaEtwExporter();
 
-    run_all_tests();
-
+    TestTraceWithGenevaEtwExporter();
   
-  std::cout << "\n\n End of Main() \n\n";
-
-  return 0;
+    std::cout << "\n\n End of Main() \n\n";
+    return 0;
 }
-
